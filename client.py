99a806841f56c35eaf0e8b25e8c6fb9d4153de23
zerozerotwo1231221 <72455712+zerozerotwo1231221@users.noreply.github.com>
2021-09-26 10:32:14 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,52 @@
+import threading
+import socket
+import pickle
+
+class Client:
+    nama = input(str('masukkan nama'))
+    pickle_out=open("Serialize","wb")
+    pickle.dump(nama,pickle_out)
+    pickle_out.close()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 59000
+    client.connect((host, port))
+obj=Client()
+
+def clien_menerima():
+    while True:
+        try:
+            message = obj.client.recv(5024).decode('utf-8')
+            if message == 'nama?':
+                obj.client.send(obj.nama.encode('utf-8'))
+            else:
+                print(message)
+                #pickle_out = message
+                pickle_out = open("Serialize","wb")
+                #example_dict = pickle.load((pickle_in))
+                pickle.dump(message,pickle_out)
+                pickle_out.close()
+        except:
+            print('error')
+            obj.client.close()
+            break
+
+
+
+def client_mengirim():
+    while True:
+
+        menginput=input("")
+        message = f'{obj.nama}:{menginput}'
+        #pickle_out=open("Serialize","wb")
+        #pickle.dump(message,pickle_out)
+        obj.client.send(message.encode())
+        #pickle_out.close()
+def THREAD():
+    receive_Thread = threading.Thread(target = clien_menerima)
+    receive_Thread.start()
+
+    send_Thread = threading.Thread(target=client_mengirim)
+    send_Thread.start()
+
+THREAD()
---
+++ server.py
@@ -0,0 +1,70 @@
+import socket
+import threading
+import pickle
+
+class Server:
+    host = socket.gethostname()
+    port = 59000
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+    clients = []
+    nama = []
+obj=Server()
+
+def siaran(message, currentClient):
+    pickle_in=open("Serialize","rb")
+
+    example_dict=pickle.load(pickle_in)
+    example_dict=message
+    file2 = open("data/riwayat.txt", "a")
+    file2.write(str(example_dict) + "\n")
+    for client in obj.clients:
+       if(client != currentClient):
+           client.send(example_dict)
+    file2.close()
+
+def mengurus_client(client):
+    while True:
+        try:
+
+            message = client.recv(5024)
+            siaran(message, client)
+            print(message)
+            pickle_out = open("Serialize", "wb")
+            pickle.dump(message, pickle_out)
+            pickle_out.close()
+        except:
+
+            index = obj.clients.index(client)
+            obj.clients.remove(client)
+            client.close()
+
+            alias = obj.nama[index]
+            keluar= (f'{alias} telah meninggalkan chat'.encode())
+            siaran(keluar, 0)
+            obj.nama.remove(alias)
+            break
+
+def menerima():
+    while True:
+        print('Server telah dimulai dan berjalan')
+        client, address = obj.server.accept()
+        print('tersambung dengan: '+str(address))
+        client.send('nama?'.encode())
+        isi_nama = client.recv(1024)
+        obj.nama.append(isi_nama)
+        obj.clients.append(client)
+
+
+        message = (f'{isi_nama} telah terhubung'.encode())
+        print(message)
+        siaran(f'{isi_nama} telah terhubung ke chat room'.encode(),0)
+        pickle_out=open("Serialize","wb")
+        pickle.dump(message,pickle_out)
+        pickle_out.close()
+        thread = threading.Thread(target = mengurus_client, args=(client, ))
+        thread.start()
+
+menerima()
